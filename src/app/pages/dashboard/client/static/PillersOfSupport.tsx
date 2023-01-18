e5505d0ac477805162c06393e3ad189dd0329bec
vishu47 <mauryavishnu98@gmail.com>
2023-01-18 22:04:40 +0530

    tu

--- src/app/pages/dashboard/client/static/PillersOfSupport.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-export default function PillersOfSupport({}: Props) {
-  return <div>PillersOfSupport</div>
-}
--- src/app/pages/dashboard/client/static/UnderTheHood.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-export default function UnderTheHood({}: Props) {
-  return <div>UnderTheHood</div>
-}
